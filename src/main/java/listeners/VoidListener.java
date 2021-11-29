36528f7c3ab9367ac6eb0c7f9d72daf064b80885
GoldcnDev <44988826+GoldcnDev@users.noreply.github.com>
2021-11-29 10:54:09 -0500

    Small change in void manager.

--- src/main/java/listeners/VoidListener.java
+++ src/main/java/listeners/VoidListener.java
@@ -23,14 +23,15 @@ public class VoidListener implements Listener {
             Player player = (Player) event.getEntity();
             if (event.getCause() == EntityDamageEvent.DamageCause.VOID) {
                 event.setCancelled(true);
-                for (World w : Bukkit.getServer().getWorlds()) {
-                    if(w.getName().equalsIgnoreCase("Slaparoo")){
+
+                Bukkit.getWorlds().forEach(world -> {
+                    if(world.getName().equalsIgnoreCase("Slaparoo")){
                         for (int x = -10000000; x < 1000000; ) {
                             for (int y = -10000000; y < 1000000; ) {
                                 for (int z = -10000000; z < 1000000; ) {
-                                    w.getBlockAt(x, y, z);
-                                    if(w.getBlockAt(x, y, z).getType() == Material.PURPLE_TERRACOTTA){
-                                        Block b = w.getBlockAt(x, y, z);
+                                    world.getBlockAt(x, y, z);
+                                    if(world.getBlockAt(x, y, z).getType() == Material.PURPLE_TERRACOTTA){
+                                        Block b = world.getBlockAt(x, y, z);
                                         purpleTerraBlock.add(b);
                                         purpleTerracotta = new Location(b.getWorld(), b.getX(), b.getY()+2, b.getZ());
                                         player.teleport(purpleTerracotta);
@@ -40,11 +41,11 @@ public class VoidListener implements Listener {
                             }
                         }
                     }
+                });
+
+
                 }
-
-
             }
         }
 
-    }
 }
