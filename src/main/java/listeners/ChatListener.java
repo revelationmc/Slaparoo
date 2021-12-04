26405a30eadfa501c06949bd9d646f41d277fbb0
GoldcnDev <44988826+GoldcnDev@users.noreply.github.com>
2021-12-04 10:00:36 -0500

    Small stuff

--- src/main/java/listeners/ChatListener.java
+++ src/main/java/listeners/ChatListener.java
@@ -22,28 +22,28 @@ public class ChatListener implements Listener {
             main.insertData(p);
         } else {
             if(main.getPoints(p) < 10){
-                e.setFormat(ColourUtils.colour("&8Baby Faced " + name + "&8\u00BB &r" + e.getMessage()));
+                e.setFormat(ColourUtils.colour("&8Baby Faced " + name + " &8\u00BB &r" + e.getMessage()));
             }
             if(main.getPoints(p) >= 10 && main.getPoints(p) < 50){
-                e.setFormat(ColourUtils.colour("&7Red Faced " + name + "&8\u00BB &r" + e.getMessage()));
+                e.setFormat(ColourUtils.colour("&7Red Faced " + name + " &8\u00BB &r" + e.getMessage()));
             }
             if(main.getPoints(p) >= 50 && main.getPoints(p) < 100){
-                e.setFormat(ColourUtils.colour("&fShazam " + name + "&8\u00BB &r" + e.getMessage()));
+                e.setFormat(ColourUtils.colour("&fShazam " + name + " &8\u00BB &r" + e.getMessage()));
             }
             if(main.getPoints(p) >= 100 && main.getPoints(p) < 250){
-                e.setFormat(ColourUtils.colour("&ePow " + name + "&8\u00BB &r" + e.getMessage()));
+                e.setFormat(ColourUtils.colour("&ePow " + name + " &8\u00BB &r" + e.getMessage()));
             }
             if(main.getPoints(p) >= 250 && main.getPoints(p) < 500){
-                e.setFormat(ColourUtils.colour("&aWallop " + name + "&8\u00BB &r" + e.getMessage()));
+                e.setFormat(ColourUtils.colour("&aWallop " + name + " &8\u00BB &r" + e.getMessage()));
             }
             if(main.getPoints(p) >= 500 && main.getPoints(p) < 750){
-                e.setFormat(ColourUtils.colour("&2Splat " + name + "&8\u00BB &r" + e.getMessage()));
+                e.setFormat(ColourUtils.colour("&2Splat " + name + " &8\u00BB &r" + e.getMessage()));
             }
             if(main.getPoints(p) >= 750 && main.getPoints(p) < 1000){
-                e.setFormat(ColourUtils.colour("&dClank " + name + "&8\u00BB &r" + e.getMessage()));
+                e.setFormat(ColourUtils.colour("&dClank " + name + " &8\u00BB &r" + e.getMessage()));
             }
             if(main.getPoints(p) >= 1000 && main.getPoints(p) < 1500){
-                e.setFormat(ColourUtils.colour("&bKapow " + name + "&8\u00BB &r" + e.getMessage()));
+                e.setFormat(ColourUtils.colour("&bKapow " + name + " &8\u00BB &r" + e.getMessage()));
             }
         }
 
